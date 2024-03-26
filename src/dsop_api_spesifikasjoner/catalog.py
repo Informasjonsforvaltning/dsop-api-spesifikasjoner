8d457b6d564041d96f86da65f9a1183e8e0dcb54
Nils Ove Tendenes <nils.ove@tendenes.com>
2024-03-26 16:05:39 +0100

    chore: provide predefined api ids in the csv file

--- src/dsop_api_spesifikasjoner/catalog.py
+++ src/dsop_api_spesifikasjoner/catalog.py
@@ -6,9 +6,16 @@ from typing import List
 class API:
     """Class representing a json dataservice (API)."""
 
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, predefined_id: str) -> None:
         """Inits an API."""
-        self.identifier = "https://dataservice-publisher.digdir.no/dataservices/{id}"
+        api_id = (
+            predefined_id
+            if len(predefined_id) > 0
+            else hashlib.sha1(str.encode(url), usedforsecurity=False).hexdigest()
+        )
+        self.identifier = (
+            f"https://dataservice-publisher.digdir.no/dataservices/{api_id}"
+        )
         self.url = url
         self.conformsTo: List[str] = []
         self.publisher = ""
@@ -22,8 +29,12 @@ class Catalog:
         catalog_title = "DSOP API katalog"
         if not production:
             catalog_title = catalog_title + " [TEST]"
-        id = hashlib.sha1(str.encode(catalog_title)).hexdigest()  # noqa: S303,S324
-        self.identifier = f"https://dataservice-publisher.digdir.no/catalogs/{id}"
+        catalog_id = hashlib.sha1(
+            str.encode(catalog_title), usedforsecurity=False
+        ).hexdigest()
+        self.identifier = (
+            f"https://dataservice-publisher.digdir.no/catalogs/{catalog_id}"
+        )
         self.title = {"nb": catalog_title}
         self.description = {"nb": "Samling av kontoopplysnings API"}
         self.publisher = "https://organization-catalog.fellesdatakatalog.digdir.no/organizations/991825827"  # noqa: B950
